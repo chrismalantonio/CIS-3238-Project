9ab39444fbdf47f6b5baf43a47cbc5d3d37a1122
ohn0 <silverskinx@gmail.com>
2017-11-15 20:25:31 -0500

    I hate git.

--- src/card_deck/Gofish.java
+++ src/card_deck/Gofish.java
@@ -19,7 +19,6 @@ public class Gofish extends Game{
     public final int DEFAULT_DECK_SIZE = 7;
     private boolean GAME_COMPLETE = false;
     
-    
     public Gofish(Deck d) {
         super(d);
         AI = new GofishPlayer[3];
@@ -47,8 +46,6 @@ public class Gofish extends Game{
         return false;
     }
     
-    
-    
     public boolean giveCardToPlayer(Card c, GofishPlayer to, GofishPlayer from){
         if(!from.hand.contains(c)){
             return false;
@@ -71,11 +68,39 @@ public class Gofish extends Game{
                 ai.draw(d);
             }
         }
-//        
-//        while(!GAME_COMPLETE){
-//            
-//            
-//        }
+        /**
+         * Create a while loop that cycles through each player.
+         * The AI players will all have the same behavior, so nothing
+         * special with them. We can just script their actions, something
+         * like: Create a buffer that holds the memory of the last N turns for
+         * all players. We can tweak N and make it bigger to increase the AI's 
+         * difficulty.
+         * 
+         * Examine each buffer and see if any card value from the buffer is the
+         * same as a value in the AI's hand.
+         * If that is the case, ask that player for the card, and get it if they
+         * still have it, or draw a card from the pool if the other player
+         * doesn't have the card.
+         * Once that is done, see if any books can be formed, and if so, remove
+         * those cards from the AI's hand and place them in the book list. That
+         * ends the AI player's turn.
+         * 
+         * The player's case requires entering another while loop that will wait
+         * for the player to make a move. When it is the player's turn, they might
+         * want to see what books each AI has completed, as well as decide which
+         * AI to ask a card from.
+         * The while loop will wait for the player to choose an AI player and
+         * request a card. It will then give the player the card if the AI has it
+         * or make the player draw a card from the pool if the AI doesn't have 
+         * the card.
+         * Once the transfer is complete, the game will attempt to create a book 
+         * from the player's hand and remove the cards from the player's hand, 
+         * placing them into the player's book list.
+         * After that, the player's turn will end and we cycle through all the AI 
+         * players again
+         */
+        
+        
         return 0;
     }
 }
