c4cc6db8b4656ff727effc899557bd71d4032ed2
Chris <Chris@DESKTOP-FO2V59B.fios-router.home>
2017-12-10 14:13:18 -0500

    Revert "removed unused function"
    This reverts commit 860603c43b04f4d8168b8d1d466230c253fd96e2.

--- src/card_deck/Game.java
+++ src/card_deck/Game.java
@@ -11,14 +11,22 @@ package card_deck;
  */
 public class Game {
 
+    public int[] record = new int[2];
     public int wins, losses;
 
     Deck d;
     public Game() {
+        record[0] = wins;
+        record[1] = losses;
     }
     
     public Game(Deck d){
         this();
         this.d = d;
+    }
+
+    public void updateRecord() {
+        record[0] = wins;
+        record[1] = losses;
     }
 }
